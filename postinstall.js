bb91c9150dcb63c8236abeec3ac44312daac5ccc
gautham-juego <gautham.rao@juegostudioz.com>
2021-09-16 13:52:49 +0530

    Postinstall bug fixes

--- postinstall.js
+++ postinstall.js
@@ -2,10 +2,11 @@
 const fs = require('fs');
 const path = require('path');
 const LIBRARY_NAME = process.env.npm_package_name;
+const EXEC_DIR = process.argv.slice(2)[process.argv.slice(2).indexOf("--init-dir") + 1];
 
 const initEnv = async () => {
-  let envFileContents = fs.readFileSync(path.resolve(process.cwd(), `node_modules/${LIBRARY_NAME}/env.json`), 'utf8');
-  let projectEnvFileContents = fs.readFileSync(path.resolve(process.cwd(), `src/config/config.json`), 'utf8');
+  let envFileContents = fs.readFileSync(path.resolve(EXEC_DIR, `node_modules/${LIBRARY_NAME}/env.json`), 'utf8');
+  let projectEnvFileContents = fs.readFileSync(path.resolve(EXEC_DIR, `src/config/config.json`), 'utf8');
   envFileContents = JSON.parse(envFileContents);
   projectEnvFileContents = JSON.parse(projectEnvFileContents);
 
@@ -20,7 +21,7 @@ const initEnv = async () => {
     projectEnvFileContents[configKey] = envFileContents;
   }
 
-  fs.writeFileSync(path.resolve(process.cwd(), `src/config/config.json`), JSON.stringify(projectEnvFileContents, null, 2), 'utf8');
+  fs.writeFileSync(path.resolve(EXEC_DIR, `src/config/config.json`), JSON.stringify(projectEnvFileContents, null, 2), 'utf8');
 }
 
 initEnv();
