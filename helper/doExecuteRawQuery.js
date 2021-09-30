ab68e02c54110c7d49ba2ead7fe7a213cb9cf179
gautham-juego <gautham.rao@juegostudioz.com>
2021-09-30 12:20:56 +0530

    Fixed update and delete raw query error

--- helper/doExecuteRawQuery.js
+++ helper/doExecuteRawQuery.js
@@ -1,9 +1,10 @@
 const { getSQLConnection } = require('./dbConnect');
+const { QueryTypes } = require('sequelize');
 
 module.exports.doExecuteRawQuery = async (sqlQuery, replacements) => {
   const conn = await getSQLConnection();
   const res = await conn.query(sqlQuery, {
-    replacements: replacements, raw: true, nest: true
+    replacements: replacements, type: QueryTypes.RAW, raw: true, nest: true
   });
   return res;
 };
