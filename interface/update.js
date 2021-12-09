93c21fe8d53304d7e6612cddd8569a883abe14cc
Dattha Prasad @Juego Studios <93972246+datthaprasad-juego@users.noreply.github.com>
2021-12-09 12:45:14 +0530

    Update update.js

--- interface/update.js
+++ interface/update.js
@@ -23,7 +23,7 @@ module.exports.update = async (tableName, query, updates) => {
           break;
 
         case '$dec':
-          eq = ` = "${key}" + `;
+          eq = ` = "${key}" - `;
           value = value[fieldVal];
           break;
       }
@@ -37,43 +37,64 @@ module.exports.update = async (tableName, query, updates) => {
   }
 
   keys = Object.keys(query);
+  let isIn=false;
+
   for (let i = 0; i < keys.length; i++) {
     if (i == 0) sql += " WHERE ";
-
     let key = keys[i];
-    let value = query[key];
+    let fixedvalue=value = query[key];
     let eq = '=';
 
+    // if value is object , i.e {$in: [1,2,3],$gte:5} then we need to check if it is $in or $gte
     if (typeof value == 'object') {
-      const fieldVal = Object.keys(value)[0];
-      switch (fieldVal) {
+      //for each condition in value, ($in,$gte,$lte,$gt,$lt)
+      Object.keys(fixedvalue).forEach((condition, k) => {
+      switch (condition) {
         case '$lt':
           eq = '<';
-          value = value[fieldVal];
+          value = fixedvalue[condition];
           break;
-
         case '$lte':
           eq = '<=';
-          value = value[fieldVal];
+          value = fixedvalue[condition]
           break;
 
         case '$gt':
           eq = '>';
-          value = value[fieldVal];
+          value = fixedvalue[condition]
           break;
 
         case '$gte':
-          eq = '>=';
-          value = value[fieldVal];
+         eq = '>=';
+          value = fixedvalue[condition]
+          break;
+
+        case '$in':
+          eq = 'IN';
+          // value = `(${fixedvalue[condition].join(',')})`;
+          value = `(${fixedvalue[condition].map(d => `'${d}'`).join(',')})`;
+          sql += ` "${key}" ${eq} ${value} ${k != Object.keys(fixedvalue).length-1 ? ' AND ' : ''}`; //if 'IN' case then instead of ?, add full array directly
+          isIn=true;
           break;
       }
 
-      if (typeof value == 'object')
-        value = JSON.stringify(value);
-    }
+      //if not 'IN' case then add ?
+      if(!isIn){
+        if (typeof value == 'object')
+          value = JSON.stringify(value);
 
-    sql += ` "${key}" ${eq} ? ${i != keys.length - 1 ? ' AND ' : ''}`;
-    replacements.push(value);
+        sql += ` "${key}" ${eq} ? ${k != Object.keys(fixedvalue).length - 1 ? ' AND ' : ''}`;
+        replacements.push(value)
+      }
+      isIn=false;
+     });
+    }
+    else{  //if not object then it is direct value as a string. i.e user_id=1
+      sql += ` "${key}" ${eq} ? ${i != keys.length - 1 ? ' AND ' : ''}`;
+      replacements.push(value);
+    }
+     sql += ` ${i != keys.length - 1 ? ' AND ' : ''}`;
+    
   }
 
   // Remove double qoutes from mysql query and replace single qoutes to double
@@ -82,7 +103,7 @@ module.exports.update = async (tableName, query, updates) => {
     sql = sql.replace(/'/g, '"');
   } else if (DATABASE_TYPE == "mysql")
     sql = sql.replace(/"/g, '');
-
+  console.log({sql,replacements});
   let res = await conn.query(sql, {
     replacements: replacements, raw: true
   });
